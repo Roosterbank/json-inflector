af4e1a918246c9a1bb58cf8b8220e7b7a3024537
Jon Smart <jonsmart@me.com>
2017-07-27 14:38:39 +0100

    Added camelCase and pascalCase as methods. Added a Typescript definition.

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+decalre module 'json-inflector' {
+    import inflection = require('inflection');
+    interface options {
+        response:string | string[];
+        request: string | string[];
+    }
+    function inflector(opts:options):any;
+    namespace inflector {
+        function inflectObject():any;
+        function transform():any;
+        function wrappedJson():any;
+    }
+    export = inflector;
+}
